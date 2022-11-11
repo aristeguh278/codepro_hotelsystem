b6f9ba2c2f1c002d033150ed1437e66433a4daf0
aristeguh278 <aristeguh278@gmail.com>
2022-11-12 02:10:02 +0700

    PART 1 SETUP_CONTEXT_API

---
+++ src/components/Navbar.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  AppBar,
+  Container,
+  IconButton,
+  Menu,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+} from "@mui/material";
+import Profile from "../resources/images/profile.jpg";
+import { Lock } from "@mui/icons-material";
+import { useValue } from "../context/ContextProvider";
+import UserIcons from "./user/UserIcons";
+
+const user = {
+  name: "test",
+  photo: Profile,
+};
+
+const Navbar = () => {
+  const {
+    state: { currentUser },
+    dispatch,
+  } = useValue();
+  return (
+    <AppBar>
+      <Container maxWidth="lg">
+        <Toolbar disableGutters>
+          <Box>
+            <IconButton size="large" color="inherit">
+              <Menu />
+            </IconButton>
+          </Box>
+          <Typography
+            component="h1"
+            variant="h6"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+          >
+            You are welcome
+          </Typography>
+
+          <Typography
+            component="h1"
+            variant="h6"
+            noWrap
+            sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+          >
+            YRW
+          </Typography>
+          {!currentUser ? (
+            <Button
+              onClick={() => dispatch({ type: "UPDATE_USER", payload: user })}
+              color="inherit"
+              startIcon={<Lock />}
+            >
+              Login
+            </Button>
+          ) : (
+            <UserIcons />
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Navbar;
---
+++ src/components/user/UserIcons.jsx
@@ -0,0 +1,39 @@
+import { Mail,NotificationAddOutlined } from '@mui/icons-material'
+import { IconButton ,Badge,Box, Avatar, Tooltip} from '@mui/material'
+import React,{useState} from 'react'
+import { useValue } from '../../context/ContextProvider'
+import UserMenu from './UserMenu'
+
+const UserIcons = () => {
+    const {state:{currentUser}}= useValue();
+    const [anchorUserMenu,setAnchorUserMenu]= useState(null)
+  return (
+   <Box>
+    <IconButton size="large" color="inherit">
+        <Badge color='error' badgeContent={5}>
+            <Mail/>
+        </Badge>
+    </IconButton>
+    <IconButton size="large" color="inherit">
+        <Badge color='error' badgeContent={20}>
+          <NotificationAddOutlined/>
+        </Badge>
+    </IconButton>
+    <IconButton size="large" color="inherit">
+        <Badge color='error' badgeContent='5'>
+            <Mail/>
+        </Badge>
+    </IconButton>
+  <Tooltip title="Open browser settings">
+  <IconButton onClick={(e)=>setAnchorUserMenu(e.currentTarget)}>
+        <Avatar src={currentUser?.photoUrl} alt={currentUser?.name}>
+            {currentUser?.name?.charAt(0).toUpperCase()}
+        </Avatar>
+    </IconButton>
+  </Tooltip>
+  <UserMenu {...{anchorUserMenu,setAnchorUserMenu}} />
+   </Box>
+  )
+}
+
+export default UserIcons
